529633b16ef4d84e89c0bfc8597cd15664e78f71
Richard Teel <richteel@teelsys.com>
2016-08-09 22:52:37 -0400

    Fixed an issue with the disconnect. Socket.shutdown requires a parameter to tell it which end(s) to shutdown.

--- gpsd/__init__.py
+++ gpsd/__init__.py
@@ -251,11 +251,13 @@ def connect(host="127.0.0.1", port=2947):
         parsed = json.loads(raw)
         _parse_state_packet(parsed)
 
-def disconnect():
+
+def disconnect(how=socket.SHUT_RDWR):
     global gpsd_socket
-    gpsd_socket.shutdown()
+    gpsd_socket.shutdown(how)
     gpsd_socket.close()
     gpsd_socket = None
+
 
 def get_current():
     """ Poll gpsd for a new position
